6146fda65d27db0c1351aadac4bbc88f865d2b5b
Erik Moqvist <erik.moqvist@gmail.com>
2026-08-03 07:50:40 +0200

    Public headers?

---
+++ Sources/MetalPetalShaderTypes/MTIShaderLib.c
@@ -0,0 +1,4 @@
+// This file intentionally left empty.
+// The MetalPetalShaderTypes target exists solely to expose Metal shader headers
+// (MTIShaderLib.h, MTIShaderFunctionConstants.h) as public headers so that
+// downstream apps can #include them in their own .metal files.
